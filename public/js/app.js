754f4a96281f497475f3bccbdb54514f4c538998
joe <s60621s60621@gmail.com>
2014-09-05 19:10:53 +0800

    add photo and modify js

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,17 @@
 (function($){
 	
-	var page_postion = [];
-
-	function goPage(num){
-		var body = $("html, body");
-		body.animate({
-			scrollTop: page_postion[num].top
-		}, '500', 'swing');
-	}
-
-	function supports_html5_storage(){
-	    try {
-	        return true;
-	    } catch(e) {
-	        return false;
-	    }
-	}
+	var page = $(".page"),
+		page_postion = [],
+		goPage = function(num){
+			var body = $("html, body");
+			body.animate({
+				scrollTop: page_postion[num].top
+			}, '500', 'swing');
+		};
 
 	$(window).resize(function(){
 		page_postion = [];
-		$(".page").css({
+		page.css({
 			'height': $(window).height()
 		}).each(function(){
 			page_postion.push($(this).offset());
@@ -27,12 +19,8 @@
 	});
 
 	$(function(){
-		
-		if(!supports_html5_storage()){
-			alert('您的瀏覽器無法正常使用此服務，請更換瀏覽器或將瀏覽器升級。');
-		};
 		page_postion = [];
-		$(".page").css({
+		page.css({
 			'height': $(window).height()
 		}).each(function(){
 			page_postion.push($(this).offset());
@@ -41,7 +29,12 @@
 			'width': '100%',
 			'maxwidth': '800px',
 			'height': '568px',
-			'photo': ['public/images/keyer_effects/kp1.png', 'public/images/keyer_effects/kp2.png', 'public/images/keyer_effects/kp3.png']
+			'photo': [
+				'public/images/keyer_effects/kp1.png', 
+				'public/images/keyer_effects/kp2.png', 
+				'public/images/keyer_effects/kp3.png',
+				'public/images/keyer_effects/kp4.png'
+			]
 		});
 		$(".logo_medium").click(function(){
 			goPage(0);
